262e220a5cfedaefd76a6a5b8fa9c683b147f6a6
Hernan Torres <tecnoher@gmail.com>
2023-01-24 15:30:27 -0300

    meme working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import html2canvas from "html2canvas";
 
-function App() {
+const memes = ['decision', 'fuego', 'viejo'];
+
+const App = () => {
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
+  const [img, setImg] = useState("");
+
+  const exportImage = () => 
+  {
+    html2canvas(document.querySelector('#meme')).then(canvas => {
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = canvas.toDataURL("image/png");
+      link.click();
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label for="pet-select">Seleccione meme:</label>
+
+      <div>
+        <select name="images" id="image-select" onChange={(e) => setImg(e.target.value)}>
+          <option value="">--Please choose an option--</option>
+          {memes.length && memes.map((el) => (
+            <option value={el}>{el}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={text1}
+          onChange={(e) => setText1(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={text2}
+          onChange={(e) => setText2(e.target.value)}
+        />
+      </div>
+      <div id="meme">
+        <span>{text1}</span> <br />
+        <span>{text2}</span> <br />
+        {img !== '' && <img src={`/img/${img}.jpg`} />}        
+      </div>
+      <button onClick={exportImage}>Exportar</button>
     </div>
   );
-}
+};
 
 export default App;
